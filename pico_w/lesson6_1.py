339b5a133499ef1d2ad0e2313e55dbb27865d2e9
hsushshudoart <hsush.han@gmail.com>
2024-01-12 14:28:34 +0800

    led燈_亮_暗

--- pico_w/lesson6_1.py
+++ pico_w/lesson6_1.py
@@ -2,7 +2,12 @@
 
 import machine
 import time
+from machine import Pin
 
+led25 = Pin("LED", Pin.OUT)
 while True:
     print(machine.freq(),"MHz")
+    led25.value(1)
+    time.sleep(1)
+    led25.value(0)
     time.sleep(1)
