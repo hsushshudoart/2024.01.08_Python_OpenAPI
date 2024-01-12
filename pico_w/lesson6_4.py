544d4e990b52309e24fd196bfa4cb9227ca5226d
hsushshudoart <hsush.han@gmail.com>
2024-01-12 16:27:07 +0800

    Timer_toggle_計時_燈一閃一閃

---
+++ pico_w/lesson6_4.py
@@ -0,0 +1,13 @@
+#自定義funtion.toggle() ->>執行反過來的行為，例:亮->暗 or 暗->亮
+# machine module
+#載入 Pin 和 Timer 的class(實體)
+
+from machine import Pin, Timer
+
+led25 = Pin("LED", Pin.OUT)
+    
+def second1(t):
+    print("過1秒")
+    led25.toggle()
+
+tim1 = Timer(period=1000, mode=Timer.PERIODIC, callback=second1)
