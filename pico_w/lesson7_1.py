8524a66a8feabdf96bcc5760c6d4cf08a543c0dc
hsushshudoart <hsush.han@gmail.com>
2024-01-15 09:53:18 +0800

    thonny_7_1

---
+++ pico_w/lesson7_1.py
@@ -0,0 +1,19 @@
+#自定義funtion.toggle() ->>執行反過來的行為，例:亮->暗 or 暗->亮
+# machine module
+#載入 Pin 和 Timer 的class(實體)
+
+from machine import Pin, Timer
+
+led25 = Pin("LED", Pin.OUT)
+    
+i = 0
+def second1(t):
+    global i
+    print("過1秒")
+    led25.toggle()
+    i = i + 1
+    if (i>=3):
+        t.deinit()
+
+tim1 = Timer()
+tim1.init(period=1000, callback=second1)
