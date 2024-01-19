9b9f3c38e00fd2eb0f498131eb772fe35ee85bab
hsushshudoart <hsush.han@gmail.com>
2024-01-19 16:11:18 +0800

    main.py_新增進入點通道_自訂值每次新增多少

--- openAPI/main.py
+++ openAPI/main.py
@@ -16,6 +16,11 @@ def read_root():
     counter = redis_conn.incr('test:increment', 1)
     return {'Counter': counter}
 
+@app.get("/counter/{c}")
+def counter(c:int):
+    counter = redis_conn.incr('test:increment', c)
+    return {'Counter': counter}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
