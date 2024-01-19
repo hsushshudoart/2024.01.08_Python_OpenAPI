24e514a9657e1f944b625d24a470446974b19cb2
hsushshudoart <hsush.han@gmail.com>
2024-01-19 15:49:17 +0800

    進到main.py_實際連到網路上去執行

--- openAPI/main.py
+++ openAPI/main.py
@@ -2,12 +2,19 @@ from typing import Union
 
 from fastapi import FastAPI
 
+import redis
+import os
+from dotenv import load_dotenv
+load_dotenv()
+redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    counter = redis_conn.incr('test:increment', 1)
+    return {'Counter': counter}
 
 
 @app.get("/items/{item_id}")
