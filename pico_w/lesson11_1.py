6af292ecd886d2e7fea67ccbf570c0413d420b12
hsushshudoart <hsush.han@gmail.com>
2024-01-22 15:42:55 +0800

    pico_w_按按鈕才亮燈_下拉電阻

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -1,4 +1,13 @@
 from machine import Pin
+import time
+
 red_led = Pin(15, mode=Pin.OUT)
-red_led.value(1)
-red_led.value(0)
+btn = Pin(14, mode=Pin.PULL_DOWN)		#PULL_DOWN 下拉電阻
+
+while True:
+    if btn.value():
+        #print("按鈕按下")
+        red_led.value(1)
+    else:
+        #print("放開按鈕")
+        red_led.value(0)
