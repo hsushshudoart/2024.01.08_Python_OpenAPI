f293cefb5dd58edc6ed0845d26b670fd98dad91a
hsushshudoart <hsush.han@gmail.com>
2024-01-22 16:48:13 +0800

    pico_w_lesson11_1_連接wifi_傳送按鈕狀態_到render雲端server

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -1,13 +1,37 @@
 from machine import Pin
 import time
 
+from tools import connect,reconnect
+import urequests
+
 red_led = Pin(15, mode=Pin.OUT)
 btn = Pin(14, mode=Pin.PULL_DOWN)		#PULL_DOWN 下拉電阻
+is_press = False						#按鈕 預設為 沒有按下，數值為0
+
+connect()
 
 while True:
     if btn.value():
-        #print("按鈕按下")
+        #按鈕按下
+        is_press = True
         red_led.value(1)
     else:
-        #print("放開按鈕")
+        #放開按鈕
+        if is_press == True:
+            print('release')
+            is_press = False
+            url_str = 'https://openapi-h8a3.onrender.com/pico_w/2024-01-22 16:02:10?address=Chicken&celsius=15.38'      #網址不能有中文字
+            
+            try:
+                response = urequests.get(url_str)            
+            except:
+                print("ap出現問題")            
+                reconnect()
+            else:
+                if response.status_code == 200:            
+                    print("傳送訊息成功")
+                else:
+                    print("傳送失敗(server出現錯誤)")
+                response.close()
+            
         red_led.value(0)
