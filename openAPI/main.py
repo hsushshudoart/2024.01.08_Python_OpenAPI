3e07b9daa491c46d5958683d31aae51f237c7c67
hsushshudoart <hsush.han@gmail.com>
2024-01-24 15:05:55 +0800

    main.py_連接到Redis

--- openAPI/main.py
+++ openAPI/main.py
@@ -35,7 +35,7 @@ async def read_item(date:str ,address:str,celsius:float=0.0):
     print(f"攝氏:{celsius}")
     return {"狀態":"儲存成功"}
 '''
-
+'''
 #給pico_w用的
 @app.get("/pico_w/{date}")
 async def read_item(date:str ,address:str,celsius:float,light:float):       #celsius:float ->>沒有給預設值,在執行的時候一定要有這個值才可以跑
@@ -43,4 +43,29 @@ async def read_item(date:str ,address:str,celsius:float,light:float):       #cel
     print(f"位置:{address}")
     print(f"攝氏:{celsius}")
     print(f"亮度:{light}")
+    return {"狀態":"儲存成功"}
+'''
+
+
+#連接到Redis資料庫
+@app.get("/pico_w/{date}")
+async def read_item(date:str ,address:str,celsius:float,light:float):       #celsius:float ->>沒有給預設值,在執行的時候一定要有這個值才可以跑
+    #print(f"日期:{date}")
+    redis_conn.rpush('pico_w:date', date)
+    #print(f"位置:{address}")
+    redis_conn.hset('pico_w:address',mapping={date:address})
+    #print(f"攝氏:{celsius}")
+    redis_conn.hset('pico_w:temperature',mapping={date:celsius})
+    #print(f"亮度:{light}")
+    redis_conn.hset('pico_w:light',mapping={date:light})
+
+    date_get = redis_conn.lrange('pico_w:date', -1,-1)[0].decode()      #('pico_w:date', -1,-1) ->>從 第-1個 到 第-1個
+    address_get = redis_conn.hget('pico_w:address',date_get).decode()
+    temperature_get = redis_conn.hget('pico_w:temperature',date_get).decode()
+    light_get = redis_conn.hget('pico_w:light',date_get).decode()
+    print(date_get)
+    print(address_get)
+    print(temperature_get)
+    print(light_get)
+
     return {"狀態":"儲存成功"}
