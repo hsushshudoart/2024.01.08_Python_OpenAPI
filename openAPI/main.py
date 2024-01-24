6d77363a717861818c0d88a2670869bdf1db0b1e
hsushshudoart <hsush.han@gmail.com>
2024-01-24 11:58:47 +0800

    main.py_新增 亮度 偵測

--- openAPI/main.py
+++ openAPI/main.py
@@ -26,11 +26,21 @@ def counter(c:int):
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 """
-
+'''
 #給pico_w用的
 @app.get("/pico_w/{date}")
 async def read_item(date:str ,address:str,celsius:float=0.0):
     print(f"日期:{date}")
     print(f"位置:{address}")
     print(f"攝氏:{celsius}")
+    return {"狀態":"儲存成功"}
+'''
+
+#給pico_w用的
+@app.get("/pico_w/{date}")
+async def read_item(date:str ,address:str,celsius:float,light:float):       #celsius:float ->>沒有給預設值,在執行的時候一定要有這個值才可以跑
+    print(f"日期:{date}")
+    print(f"位置:{address}")
+    print(f"攝氏:{celsius}")
+    print(f"亮度:{light}")
     return {"狀態":"儲存成功"}
