29a88584796a93a306c999674be8a8cf92067ed6
hsushshudoart <hsush.han@gmail.com>
2024-01-24 10:02:30 +0800

    定義get板子溫度function_print現在溫度

---
+++ pico_w/lesson11_2.py
@@ -0,0 +1,63 @@
+from machine import Pin
+import time
+
+from tools import connect,reconnect
+import urequests
+
+#red_led = Pin(15, mode=Pin.OUT)
+#btn = Pin(14, mode=Pin.PULL_DOWN)		#PULL_DOWN 下拉電阻
+red_led = Pin(15, Pin.OUT)
+btn = Pin(14, Pin.IN , Pin.PULL_DOWN)
+is_press = False						#按鈕 預設為 沒有按下，數值為0
+
+#connect()
+
+def getCurrentTime():
+    times_tuple = time.localtime()
+    currentTime = f'{times_tuple[0]}-{times_tuple[1]}-{times_tuple[2]} {times_tuple[3]}:{times_tuple[4]}:{times_tuple[5]}'
+    return currentTime
+
+def getTemperature():
+    sensor_temp = machine.ADC(4)
+    conversion_factor = 3.3 / (65535)
+    reading = sensor_temp.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    return temperature
+
+while True:
+    #按鈕按下
+    if btn.value() == True:	#== True可省略
+        #解決按鈕按下彈跳
+        time.sleep_ms(50)	#等待0.05秒
+        if btn.value():
+            is_press = True
+            red_led.value(1)
+    else:
+        #放開按鈕
+        #red_led.value(0)  #在這裡,燈會馬上反應,但是雲端伺服器不確定完成與否
+        #解決按鈕放開彈跳
+        time.sleep_ms(50)	#等待0.05秒
+        if btn.value() == False:
+            if is_press == True:
+                print('release')
+                print(getCurrentTime())
+                print(getTemperature())
+                print('')
+                is_press = False
+                '''
+                url_str = 'https://openapi-h8a3.onrender.com/pico_w/2024-01-22 16:02:10?address=Chicken&celsius=15.38'      #網址不能有中文字
+                
+                try:
+                    response = urequests.get(url_str)            
+                except:
+                    print("ap出現問題")            
+                    reconnect()
+                else:
+                    if response.status_code == 200:            
+                        print("傳送訊息成功")
+                    else:
+                        print("傳送失敗(server出現錯誤)")
+                    response.close()
+                '''
+            red_led.value(0)
+
