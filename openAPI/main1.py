f854e29e54784fc98f8da1f7cae1d0486f9bedb4
hsushshudoart <hsush.han@gmail.com>
2024-01-24 16:40:37 +0800

    main1.py_新增pydantic 和 一個class

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 import redis
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
+
 load_dotenv()
 redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 
@@ -73,7 +75,7 @@ async def read_item(date:str ,address:str,celsius:float,light:float):       #cel
 
 
 
-
+'''
 #測試 ->> 給pico_w用的
 @app.get("/pico_w/{date}")
 async def read_item(date:str ,address:str,celsius:float=0.0):       #celsius:float=0.0 ->>有給預設值,在執行的時候沒有這個值也可以跑
@@ -81,3 +83,47 @@ async def read_item(date:str ,address:str,celsius:float=0.0):       #celsius:flo
     print(f"位置:{address}")
     print(f"攝氏:{celsius}")
     return {"狀態":"儲存成功"}
+'''
+
+'''
+#測試 ->> qeury parameter
+@app.get("/pico_w/")
+async def read_item(count:int=1):
+    date_get = redis_conn.lrange('pico_w:date', -1,-1)[0].decode()      #('pico_w:date', -1,-1) ->>從 第-1個 到 第-1個
+    address_get = redis_conn.hget('pico_w:address',date_get).decode()
+    temperature_get = redis_conn.hget('pico_w:temperature',date_get).decode()
+    light_get = redis_conn.hget('pico_w:light',date_get).decode()
+    print(date_get)
+    print(address_get)
+    print(temperature_get)
+    print(light_get)
+    return {'date':date_get,
+            'address':address_get,
+            'temperature':temperature_get,
+            'light':light_get
+            }
+'''
+
+#新增一個class
+class Pico_w(BaseModel):
+    date:str
+    address:str
+    temperature:float
+    light:float
+
+#pydantic
+#用一個qeury parameter ->> count預設值是1
+@app.get("/pico_w/")
+async def read_item(count:int=1):
+    date_list = redis_conn.lrange('pico_w:date',-count,-1)
+    dates = [date.decode() for date in date_list]
+    all_Data:[Pico_w] = []
+    for date in dates:
+        address_get = redis_conn.hget('pico_w:address',date).decode()
+        temperature_get = redis_conn.hget('pico_w:temperature',date).decode()
+        light_get = redis_conn.hget('pico_w:light',date).decode()
+        item = Pico_w(date=date,address=address_get,temperature=float(temperature_get),light=float(light_get))
+        all_Data.append(item)
+
+    
+    return all_Data
