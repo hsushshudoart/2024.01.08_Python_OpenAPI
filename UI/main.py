a2e6476b481e22d6f6ef2fca4781d64348d457b9
hsushshudoart <hsush.han@gmail.com>
2024-01-26 12:00:31 +0800

    每一段時間_local介面自動刷新抓一次最新幾筆資料

--- UI/main.py
+++ UI/main.py
@@ -1,7 +1,12 @@
 import requests
 import pandas as pd
+import streamlit as st
+from streamlit_autorefresh import st_autorefresh
 
 url = 'https://openapi-h8a3.onrender.com/pico_w/?count=7'
+
+#自動reload頁面,每10秒一次
+st_autorefresh(interval=10000, limit=100, key="fizzbuzzcounter")
 
 r = requests.get(url=url)
 
@@ -10,4 +15,15 @@ if r.status_code == 200:
     data = r.json()
 
 dataFrame = pd.DataFrame(data)
-print(dataFrame)
+#print(dataFrame)
+
+st.header("學院養雞場")
+st.divider()
+st.caption("溫度 & 光線 表😍")
+st.write(dataFrame)
+st.divider()
+st.caption("光線")
+st.line_chart(dataFrame,x='date',y='light')
+st.divider()
+st.caption("溫度")
+st.line_chart(dataFrame,x='date',y='temperature',color='#ff0000')
